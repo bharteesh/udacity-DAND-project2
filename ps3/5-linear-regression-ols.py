2df1f95c7cbbd1c90048c69ae7bf4f7a121fe76e
Bharteesh Kulkarni <bharteesh.kulkarni@ithaka.org>
2015-10-16 13:15:43 -0400

    ps3 code

---
+++ ps3/5-linear-regression-ols.py
@@ -0,0 +1,39 @@
+import numpy as np
+import pandas
+import statsmodels.api as sm
+
+"""
+In this question, you need to:
+1) implement the linear_regression() procedure
+2) Select features (in the predictions procedure) and make predictions.
+
+"""
+
+def linear_regression(features, values):
+    """
+    Perform linear regression given a data set with an arbitrary number of features.
+    
+    This can be the same code as in the lesson #3 exercise.
+    """
+    features = sm.add_constant(features)
+    model = sm.OLS(values, features)
+    results = model.fit()
+    intercept = results.params[0]
+    params = results.params[1:]
+    
+    return intercept, params
+
+def predictions(dataframe):
+    
+    features = dataframe[['rain', 'precipi', 'Hour', 'meantempi','fog','meanwindspdi']]
+    dummy_units = pandas.get_dummies(dataframe['UNIT'], prefix='unit')
+    features = features.join(dummy_units)
+    
+    # Values
+    values = dataframe['ENTRIESn_hourly']
+
+    # Perform linear regression
+    intercept, params = linear_regression(features, values)
+    
+    predictions = intercept + np.dot(features, params)
+    return predictions
